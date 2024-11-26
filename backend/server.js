32bcc643b0434be3728b4901b795f797d3da0915
Emir Cokrlija <144956911+emirCo95@users.noreply.github.com>
2024-11-26 15:28:00 -0500

    auth route

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,16 @@
 import express from 'express';
 import dotenv from 'dotenv';
 
+//ROUTES
+import authRoutes from './routes/auth.route.js';
+
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+//AUTHENTICATION
+app.use('/api/auth', authRoutes);
 
 app.listen(PORT, () => {
   console.log('Server is running on http://localhost:' + PORT);
