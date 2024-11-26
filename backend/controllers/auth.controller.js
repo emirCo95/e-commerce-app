711ef876d2ded1ce5cf6131b89a0d84df7f044b0
Emir Cokrlija <144956911+emirCo95@users.noreply.github.com>
2024-11-26 17:08:36 -0500

    create controllers

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,9 @@
+export const signup = async (req, res) => {
+  res.send('Sign up Route called');
+};
+export const login = async (req, res) => {
+  res.send('Log In Route called');
+};
+export const logout = async (req, res) => {
+  res.send('Log Out Route called');
+};
